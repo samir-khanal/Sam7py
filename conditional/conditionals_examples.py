09b2939da3510b92fbafafacf49345d1bec2fce4
samir-khanal <khanalsamirk@gmail.com>
2022-05-07 21:04:03 +0545

    update

---
+++ conditional/conditionals_examples.py
@@ -0,0 +1,54 @@
+# x = 10
+# y = 20
+# z = 30
+# if x > y and x > z:
+#     print("x is greater")
+# elif y > z and y > z:
+#     print("y is greater")
+# else:
+#     print("z is large")
+
+# Question 1
+# username = input("enter a username:")
+# enter_password = input("enter password:")
+# given_username = "ram"
+# given_password = "ram002"
+# if username == given_username and enter_password == given_password or username == "admin" and enter_password == "admin002":
+#     print("Welcome to the program")
+# else:
+#     print("username and password  is wrong")
+
+# Question 2
+# age = int(input("enter your age:"))
+# if age < 18:
+#     print("you are not eligible to the party as you are child")
+# elif age >= 18 and age <= 40:
+#     print("welcome to the party")
+# else:
+#     print("you are not eligible")
+
+# Question 3
+# name = input("enter a name:")
+# print("Name is", name)
+# science = int(input("enter marks of science:"))
+# if science > 100:
+#     print("exit")
+#     exit()
+# nepali = int(input("enter marks of nepali:"))
+# social = int(input("enter marks of social:"))
+# maths = int(input("enter marks of maths:"))
+# english = int(input("enter marks of english:"))
+# total = science + nepali + social + maths + english
+# print("total marks obtained:", total)
+# per = total / 5
+# print("percentage:", per, "%")
+# if per < 35:
+#     print("you are fail")
+# elif per >= 35 and per < 45:
+#     print("you got third division")
+# elif per >= 45 and per < 60:
+#     print("you got second division")
+# elif per >= 60 and per < 75:
+#     print("you got first division")
+# elif per <= 100:
+#     print("you got distinction")
--- conditional/introduction_1.py
+++ conditional/introduction_1.py
@@ -1,65 +1,67 @@
-print("=========welcome to Computer bazaar==========")
-total_price = 0
-gt = 0
-tax_amount = 0
-quantity = 0
-print("here are the laptops that are available in our store right now\n"
-      "1.Dell(Rs:20000)\n"
-      "2.Toshiba(Rs:30000)\n"
-      "3.Mac(Rs:50000)")
-dell = 0
-toshiba = 0
-mac = 0
-lap = int(input("Select any option:"))
-if lap == 1:
-    quantity = int(input("quantity:"))
-    dell = 20000 * quantity
-elif lap == 2:
-    quantity = int(input("quantity:"))
-    toshiba = 30000 * quantity
-elif lap == 3:
-    quantity = int(input("quantity:"))
-    mac = 50000 * quantity
-else:
-    print("Exit")
-    exit()
+# print("=========welcome to Computer bazaar==========")
+# total_price = 0
+# gt = 0
+# tax_amount = 0
+# quantity = 0
+# print("here are the laptops that are available in our store right now\n"
+#       "1.Dell(Rs:20000)\n"
+#       "2.Toshiba(Rs:30000)\n"
+#       "3.Mac(Rs:50000)")
+# dell = 0
+# toshiba = 0
+# mac = 0
+# lap = int(input("Select any option:"))
+# if lap == 1:
+#     quantity = int(input("quantity:"))
+#     dell = 20000 * quantity
+# elif lap == 2:
+#     quantity = int(input("quantity:"))
+#     toshiba = 30000 * quantity
+# elif lap == 3:
+#     quantity = int(input("quantity:"))
+#     mac = 50000 * quantity
+# else:
+#     print("Exit")
+#     exit()
+#
+# print("Delivery options: 1.home(Rs.1000) 2.Pick_up(free)")
+# deli_price = 0
+# home = 1000
+# pick_up = 0
+# deli = int(input("enter the delivery option number:"))
+# if deli == 1:
+#     deli_price += home
+# elif deli == 2:
+#     deli_price += pick_up
+#
+# print("Packaging options:\n"
+#       "a.plastic(Rs.500)\n"
+#       "b.bag(Rs.1000)\n"
+#       "c.gift_box(Rs.5000)\n"
+#       "d.none")
+# plastic = 0
+# bag = 0
+# gift_box = 0
+# pack = (input("Enter option:"))
+# if pack == "a":
+#     plastic += 500
+# elif pack == "b":
+#     bag += 1000
+# elif pack == "c":
+#     gift_box += 5000
+# else:
+#     print("none")
+#
+# print("Locations: 1.KTM(13% tax) 2.LTP(free) 3.BKT(free)")
+# ktm = (13 / 100)
+# lit = 0
+# bkt = 0
+# loc = int(input("Enter a delivery option:"))
+# total_price += dell + toshiba + mac
+# if loc == 1:
+#     tax_amount += total_price * ktm
+#
+# gt = total_price + deli_price + bag + plastic + gift_box + tax_amount
+# print(f"Total quantity:{quantity} Total price:{total_price} Tax amount:{tax_amount} Gt:{gt}")
 
-print("Delivery options: 1.home(Rs.1000) 2.Pick_up(free)")
-deli_price = 0
-home = 1000
-pick_up = 0
-deli = int(input("enter the delivery option number:"))
-if deli == 1:
-    deli_price += home
-elif deli == 2:
-    deli_price += pick_up
 
-print("Packaging options:\n"
-      "a.plastic(Rs.500)\n"
-      "b.bag(Rs.1000)\n"
-      "c.gift_box(Rs.5000)\n"
-      "d.none")
-plastic = 0
-bag = 0
-gift_box = 0
-pack = (input("Enter option:"))
-if pack == "a":
-    plastic += 500
-elif pack == "b":
-    bag += 1000
-elif pack == "c":
-    gift_box += 5000
-else:
-    print("none")
-
-print("Locations: 1.KTM(13% tax) 2.LTP(free) 3.BKT(free)")
-ktm = (13 / 100)
-lit = 0
-bkt = 0
-loc = int(input("Enter a delivery option:"))
-total_price += dell + toshiba + mac
-if loc == 1:
-    tax_amount += total_price * ktm
-
-gt = total_price + deli_price + bag + plastic + gift_box + tax_amount
-print(f"Total quantity:{quantity} Total price:{total_price} Tax amount:{tax_amount} Gt:{gt}")
