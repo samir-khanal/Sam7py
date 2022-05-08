8469aedad091e36dd89e7ff23fb1c39d9023bcfe
samir-khanal <khanalsamirk@gmail.com>
2022-05-08 08:29:07 +0545

    new

---
+++ loop/introduction.py
@@ -0,0 +1,48 @@
+# while loop:indefinite iteration
+# EX:
+# x=1
+# while x<10:
+#     print(x,end=" ")
+#     x+=1
+# end=" " is used to print output in horizontal.
+# a=1
+# nam=int(input("enter the no of times:"))
+# while a<=nam:
+#     print("samir") # or print(f{increment}: samir")
+#     a+=1
+
+# total_even = 0
+# total_odd = 0
+# x = 1
+# num = int(input("enter number:"))
+# while x <= num:
+#     if x % 2 == 0:
+#         total_even += x
+#     else:
+#         total_odd += x
+#     x += 1
+# print(f"total even:{total_even}")
+# print(f"total odd:{total_odd}")
+# #
+# data=["ram","sita","gita"]
+# for name in data:
+#     print(name)
+
+# total=0
+#
+# num=int(input("enter the no of students:"))
+# x=1
+# while x<num:
+#  print()
+# for x in range(1):
+#     sci=int(input("science marks:"))
+#     nep = int(input("science marks:"))
+#     math=int(input("science marks:"))
+#     soc = int(input("science marks:"))
+#     eng = int(input("science marks:"))
+#     total=sci+nep+math+soc+eng
+#     x += 1
+
+
+
+
