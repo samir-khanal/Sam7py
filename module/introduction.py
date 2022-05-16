5ee1d07a1ac6eafaffbe18a2d923caa7317df144
samir-khanal <khanalsamirk@gmail.com>
2022-05-16 08:06:36 +0545

    update module

---
+++ module/introduction.py
@@ -0,0 +1,12 @@
+# print("i am from introduction.py")
+
+# a=10
+#
+# def test():
+#     return"i am test function"
+#
+# def ge_std():
+#     return"i am a student"
+
+if __name__=="__main__":
+    print("my name is samir")
---
+++ module/main.py
@@ -0,0 +1,32 @@
+# import introduction
+#
+# print(introduction.test())
+# print(introduction.ge_std())
+
+# import introduction as intro
+#
+# print(intro.test())
+
+# from introduction import ge_std, test
+# print(test())
+# print(ge_std())
+#
+# * all modules
+# from introduction import *
+#
+# def run():
+#     from introduction import ge_std
+#     print(ge_std())
+#     print(test())
+#
+#
+# run()
+
+
+# print(__file__)
+# print(__name__)
+
+# import introduction
+
+
+
