456c76ad9c00ba77ccf76dbeeb14a080c85fc799
samir-khanal <khanalsamirk@gmail.com>
2022-05-18 08:26:37 +0545

    new

---
+++ exception handling/main.py
@@ -0,0 +1,25 @@
+# print(10/0)
+
+# try:
+#     print(10/0)
+# except Exception as e:
+#     print("Exception:",e)
+#
+# finally:
+#     print("finally")
+#
+# ZeroDivisionError
+# IndexError
+
+# def add(x,y):
+#     if y==0:
+#         raise Exception("y cannot be zero")
+#     return x+y
+#
+# try:
+#     print(add(10,0))
+# except Exception as e:
+#     print("Exception:",e)
+
+
+
---
+++ fileHandling/main.py
@@ -0,0 +1,37 @@
+# .txt
+# .bin
+# .csv
+# .json
+# .xml
+# .yaml
+
+# file mode
+# w- write
+# a- append
+# r+ - read and write
+# w+ - write and read
+
+# open("student.txt","w").close()
+
+# handle=open("student.txt","w")
+# handle.write("test was successful")
+# handle.close()
+
+# obj=open("student.txt","r")
+# print(obj.readline())
+# print(obj.readlines())
+# print(obj.read())
+
+#
+# with open("student.txt","r") as obj:
+#     print(obj.readlines())
+
+# open("record.txt","w").close()
+
+# a = int(input("enter a number:"))
+# b = int(input("enter a number:"))
+# sub = a + b
+#
+#
+# with open("record.txt", "w") as obj
+# obj.write("f.the sum is:")
