cb7bd6c492011dadacf94318e007385b5747ef25
samir-khanal <khanalsamirk@gmail.com>
2022-05-19 08:21:00 +0545

    another

---
+++ oop/day1/introduction.py
@@ -0,0 +1,24 @@
+# declare a class
+# class introduction:
+#     # body part of class
+#     x=10 #it is property.
+#
+#     def test(self):
+#         pass
+#
+#
+# # declare an object: instance of the class
+# obj=introduction()
+# # access the object
+# print(obj.x)
+
+
+class students:
+    def name(self, name):
+        print(f"student name is {name}")
+
+    def age(self, age):
+        print(f"student age is {age}")
+
+
+obj = students()
